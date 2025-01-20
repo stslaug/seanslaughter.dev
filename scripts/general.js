6120be5b7498932f59cf116c642ecbb24d522c96
Tyler Slaughter <stslaug@gmail.com>
2025-01-20 00:22:53 -0500

    Learned about Dynamic JavaScript Fetching.
    Made About me, Welcome, and Skills, navbar, and footer in a separate container
    
    Signed-off-by: Tyler Slaughter <stslaug@gmail.com>

---
+++ scripts/general.js
@@ -0,0 +1,45 @@
+
+// Dynamically Load the Navbar
+document.addEventListener('DOMContentLoaded', () => {
+    fetch('../components/navbar.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('navbar').innerHTML = data;
+        });
+});
+
+// Dynamically Load the Footer
+document.addEventListener('DOMContentLoaded', () => {
+    fetch('../components/footer.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('footer').innerHTML = data;
+        });
+});
+
+// Dynamically load an 'about me' component
+document.addEventListener('DOMContentLoaded', () => {
+    fetch('../components/about.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('aboutme').innerHTML = data;
+        });
+});
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    fetch('../components/skills.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('myskills').innerHTML = data;
+        });
+});
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    fetch('../components/welcome-splash.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('welcome-splash').innerHTML = data;
+        });
+});
