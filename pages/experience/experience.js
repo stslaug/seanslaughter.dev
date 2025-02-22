e1bc1f07292cfc45b4b257b52b7aef2f1fbd78be
Tyler Slaughter <stslaug@gmail.com>
2025-02-22 03:43:25 -0500

    Designed + Created a dark mode for resume
    Signed-off-by: Tyler Slaughter <stslaug@gmail.com>

---
+++ pages/experience/experience.js
@@ -0,0 +1,18 @@
+
+
+$(document).ready(function(){
+    
+    $('#darkMode-btn').click(function() {
+        console.log("Jquery Working");
+        console.log("Jquery Working");
+        $('body').toggleClass("dark-mode");
+        $('a').toggleClass("dark-mode");
+        $('strong').toggleClass("dark-mode");
+        $('h1').toggleClass("dark-mode");
+        $('h2').toggleClass("dark-mode");
+        $('h3').toggleClass("dark-mode");
+        $('#theme').toggleClass('fa-moon');
+        $('#theme').toggleClass('fa-sun');
+    
+    });
+});
