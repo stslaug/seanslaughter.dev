917f054db5e6ed2740a5bb17c5ec5504be784a3a
Tyler Slaughter <stslaug@gmail.com>
2025-02-22 13:29:50 -0500

    Completed keypress.html
    Signed-off-by: Tyler Slaughter <stslaug@gmail.com>

---
+++ class-env/pages/keypress/keypress.js
@@ -0,0 +1,20 @@
+let vowels = ['a', 'e', 'i', 'o', 'u'];
+$(document).ready(function() {
+    $(document).on("keypress", function(event) {  
+        const pressedKey = event.key.toLowerCase(); 
+        
+        if (pressedKey === 'r') {
+            $('body').css('background-color', 'indianred');
+        } else if (pressedKey === 'g') {
+            $('body').css('background-color', 'mediumseagreen');
+        } else if (pressedKey === 'b') {
+            $('body').css('background-color', 'dodgerblue');
+        }
+        if(vowels.includes(pressedKey)) {
+            $('#lastKey').text("Last Key Pressed: " + event.key.toUpperCase());
+        } else
+        {
+            $('#lastKey').text("Last Key Pressed: " + event.key);
+        }
+    });
+});
