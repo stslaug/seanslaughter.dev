5cf392ea81ac9255fcd98297349674dd438a1bd4
Tyler Slaughter <stslaug@gmail.com>
2025-02-22 14:22:27 -0500

    Completed jquery.html
    Signed-off-by: Tyler Slaughter <stslaug@gmail.com>

---
+++ class-env/pages/jquery-intro/jquery.js
@@ -0,0 +1,50 @@
+$(document).ready(function () {
+    $('div').on('mouseenter', function () {
+        $(this).css('background-color', 'yellow');
+    });
+    $('div').on('mouseleave', function () {
+        $(this).css('background-color', 'white');
+    });
+
+    
+    
+    
+    $('#fadeDiv1').on('click', function () {
+        $('#div1').fadeOut();
+    });
+
+    $('#fadeDiv2').on('click', function () {
+        $('#div2').fadeOut();
+    });
+
+    $('#fadeDiv3').on('click', function () {
+        $('#div3').fadeOut();
+    });
+
+    
+    
+    $('#datepicker').datepicker();
+
+    $('#accordion').accordion(); 
+
+    $('#progressbar').progressbar({
+        value: 0
+    });
+    
+    
+    
+
+    let progressValue = 0;
+    const totalTime = 20; // 20 seconds
+
+    const progressInterval = setInterval(function () {
+        progressValue += 1;
+        $('#progressbar').progressbar({
+            value: (progressValue / totalTime) * 100
+        });
+
+        if (progressValue >= totalTime) {
+            clearInterval(progressInterval);
+        }
+    }, 1000); // update every 1 second
+});
