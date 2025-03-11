89dce60df3aa64ba8b79cbf0c73275c8a17c3b1f
Tyler Slaughter <stslaug@gmail.com>
2025-03-11 00:48:53 -0400

    -Style Updates. Learned How to Save Darkmode state among pages which is CRAZYYYYYY and also learned how to do some cool gradient background transitions.. APplied to navbar and welcome pages.
    Signed-off-by: Tyler Slaughter <stslaug@gmail.com>

--- components/navbar/navbar.js
+++ components/navbar/navbar.js
@@ -1,32 +1,41 @@
 jQuery(function () {
-    let $head = $('head');
-    $head.append('<script src="https://kit.fontawesome.com/44ecd57f75.js" crossorigin="anonymous"></script>');
-
+    // Font Awesome setup
+    $('head').append('<script src="https://kit.fontawesome.com/44ecd57f75.js" crossorigin="anonymous"></script>');
 
     // Toggle mobile menu
     $("#hamburger").on('click', function () {
         $("#nav-items").toggleClass("active");
     });
 
-
     // Dropdown and submenu functionality (for desktop)
-    $(".dropdown").on('hover', function () {
+    $(".dropdown").hover(function () {
         $(this).find(".dropmenu").stop(true, true).slideDown();
     }, function () {
         $(this).find(".dropmenu").stop(true, true).slideUp();
     });
 
-    $(".submenu-wrap").on('hover', function () {
+    $(".submenu-wrap").hover(function () {
         $(this).find(".submenu").stop(true, true).fadeIn();
     }, function () {
         $(this).find(".submenu").stop(true, true).fadeOut();
     });
 
+    // Apply dark mode from saved preference
+    if (localStorage.getItem('darkMode') === 'true') {
+        $('body, #footer').addClass('dark-mode');
+        $('#darkmode-toggle i').removeClass('fa-moon').addClass('fa-sun');
+    }
 
+    // Dark mode toggle with improved selector targeting
     $('#darkmode-toggle').on('click', function () {
-        $('body').toggleClass('dark-mode');
-        $('#footer').toggleClass('dark-mode');
-        $('#darkmode-toggle i').toggleClass('fa-moon').toggleClass('fa-sun');
-    });
+        $('body, #footer').toggleClass('dark-mode');
+        $('#darkmode-toggle i').toggleClass('fa-moon fa-sun');
 
+        // Save preference
+        try {
+            localStorage.setItem('darkMode', $('body').hasClass('dark-mode') ? 'true' : 'false');
+        } catch (e) {
+            console.warn('Could not save dark mode preference');
+        }
+    });
 });
