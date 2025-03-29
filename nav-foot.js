80cf36ae8501ec06f45b9f47b77171045c9935d0
Tyler Slaughter <stslaug@gmail.com>
2025-03-29 19:13:08 -0400

    -Changed order which font-awesome was injected into nav-foot rather than navbar.js
    Signed-off-by: Tyler Slaughter <stslaug@gmail.com>

--- nav-foot.js
+++ nav-foot.js
@@ -1,4 +1,4 @@
-$('head').append('<script src="https://kit.fontawesome.com/44ecd57f75.js" crossorigin="anonymous"></script>');
+$('head').append('<link crossorigin = "anonymous" href = "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" integrity = "sha512-9usAa10IRO0HhonpyAIVpjrylPvoDwiPUiKdWk5t3PyolY1cOd4DSE0Ga+ri4AuTroPR5aQvXU9xC6qOPnzFeg==" referrerpolicy = "no-referrer" rel = "stylesheet"/>');
 
 jQuery(function () {
     // Dynamically Load the Navbar
